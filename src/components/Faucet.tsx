bf185cb4da8c37835ad4bcff1a66412ed1869c8d
Sanjay <sanjayrjs16@gmail.com>
2022-03-30 00:39:31 +0530

    Slider component added

--- src/components/Faucet.tsx
+++ src/components/Faucet.tsx
@@ -1,13 +1,14 @@
-import { Button, Input, VStack } from "@chakra-ui/react";
+import { Button, Input, VStack, useToast } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { PublicKey, Connection } from "@solana/web3.js";
+import SliderButton from "./SliderButton";
 
 const Faucet = () => {
-  const [address, setAddress] = useState("");
+  const [address, setAddress] = useState<any>("");
   const [isValidAddress, setIsValidAddress] = useState(false);
-
+  const toast = useToast();
+  let publicKey: PublicKey;
   const validateSolanaAddress = async (addr: string) => {
-    let publicKey;
     try {
       publicKey = new PublicKey(addr);
       return await PublicKey.isOnCurve(publicKey.toBytes());
@@ -24,9 +25,39 @@ const Faucet = () => {
     checkAddressValid();
   }, [address]);
 
-  const requestAirDrop = async (addrs: string) => {};
+  const requestAirDrop = async () => {
+    try {
+      const NODE_RPC = "https://api.testnet.solana.com"; // devnet environment
+      const CONNECTION = new Connection(NODE_RPC);
+      console.log("Calling request airdrop to address", address);
+      const confirmation = await CONNECTION.requestAirdrop(
+        publicKey,
+        1000000000
+      );
+      alert(confirmation);
+      toast({
+        position: "top",
+        title: "Airdrop succesful",
+        description: "Please check your wallet",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (err) {
+      console.log("Error: ", err);
+      toast({
+        position: "top",
+        title: "Airdrop failed",
+        description: "Something went wrong  ",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
   return (
     <VStack>
+      <SliderButton />
       <Input
         placeholder="Enter solana wallet address"
         value={address}
@@ -36,10 +67,7 @@ const Faucet = () => {
         onChange={(e: any) => setAddress(e.target.value)}
         color={"black"}
       />
-      <Button
-        onClick={() => requestAirDrop(address)}
-        disabled={!isValidAddress}
-      >
+      <Button onClick={() => requestAirDrop()} disabled={!isValidAddress}>
         Request airdrop
       </Button>
     </VStack>
--- src/components/SliderButton.tsx
+++ src/components/SliderButton.tsx
@@ -1,7 +1,50 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styles from "../../styles/Home.module.css";
+import { Box, Button, ButtonGroup, Container, Slide } from "@chakra-ui/react";
 const SliderButton = () => {
-  return <div>SliderButton</div>;
+  const [isTestNet, setIsTestNet] = useState(false);
+  return (
+    <ButtonGroup
+      style={{
+        position: "relative",
+        border: "2px solid black",
+        borderRadius: "50px",
+        overflow: "hidden",
+      }}
+    >
+      <Button
+        onClick={() => setIsTestNet(true)}
+        style={
+          isTestNet
+            ? {
+                border: "4px solid springgreen",
+                transform: "scale(1.25)",
+                backgroundColor: "springgreen",
+              }
+            : { border: "4px solid grey", backgroundColor: "grey" }
+        }
+      >
+        {isTestNet ? "✅" : ""} Testnet
+      </Button>
+
+      <Button
+        onClick={() => setIsTestNet(false)}
+        style={
+          !isTestNet
+            ? {
+                border: "4px solid springgreen",
+
+                transform: "scale(1.25)",
+                backgroundColor: "springgreen",
+                color: "white",
+              }
+            : { border: "4px solid grey", backgroundColor: "GrayText" }
+        }
+      >
+        {!isTestNet ? "✅" : ""} Devnet
+      </Button>
+    </ButtonGroup>
+  );
 };
 
 export default SliderButton;
